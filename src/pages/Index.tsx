d20252ff88612122d073a742c6a2ba9957c33b2f
kakderishikesh <rishikesh.kakde59@gmail.com>
2025-04-23 18:02:52 -0400

    Index Update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,18 +1,17 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import RishiAIChat from "../components/RishiAIChat";
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-white dark:bg-black">
       <NavBar />
-      <main className="flex-1 flex items-center justify-center text-center px-4">
-        <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
-          Welcome to Rishi-AI ✨
-        </h1>
+      <main className="flex flex-1 flex-col justify-center">
+        <RishiAIChat />
       </main>
       <Footer />
     </div>
-  );
+  )
 };
 
 export default Index;
